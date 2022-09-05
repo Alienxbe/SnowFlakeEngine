6a49e227f73e6e6deb38787187f2e95cc17db179
Alienxbe <martin.ykman@gmail.com>
2022-09-05 20:06:54 +0200

    Added function to header file: - sfe_xpm_file_to_image

--- includes/sfe_image.h
+++ includes/sfe_image.h
@@ -6,7 +6,7 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 17:29:37 by mykman            #+#    #+#             */
-/*   Updated: 2022/08/19 20:56:29 by mykman           ###   ########.fr       */
+/*   Updated: 2022/09/05 20:06:27 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 t_img	sfe_image_new(void *mlx_ptr, t_point size, t_color bg);
 t_img	sfe_image_sub(void *mlx_ptr, t_img img, t_area area);
 t_bool	sfe_image_cpy(t_img src, t_img dst, t_point p_dst);
+t_img	sfe_xpm_file_to_image(void *mlx_ptr, char *filename);
 void	sfe_image_destroy(void *mlx_ptr, t_img img);
 
 #endif
