c0ff25993606c169021fe146bcae739dd37db762
Alienxbe <martin.ykman@gmail.com>
2022-09-05 13:15:29 +0200

    SnowFlakeEngine v1 done

--- srcs/core/sfe_move.c
+++ srcs/core/sfe_move.c
@@ -1,30 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sfe.h                                              :+:      :+:    :+:   */
+/*   sfe_move.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/10 00:26:06 by mykman            #+#    #+#             */
-/*   Updated: 2022/09/05 13:14:23 by mykman           ###   ########.fr       */
+/*   Created: 2022/09/05 13:12:15 by mykman            #+#    #+#             */
+/*   Updated: 2022/09/05 13:12:47 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SFE_H
-# define SFE_H
+#include "sfe.h"
 
-# include "ft_point.h"
-# include "sfe_scene.h"
-
-# define SFE_DEFAULT_FPS_MAX	60
-
-typedef struct s_sfe	t_sfe;
-
-t_sfe	*sfe_init(const char *name, t_point size);
-void	sfe_set_active_scene(t_sfe *sfe, t_scene *scene);
-void	sfe_hook(t_sfe *sfe, int event, int (*f)(), void *param);
-void	sfe_destroy(t_sfe *sfe);
-
-void	sfe_move(t_sfe *sfe, t_point *pos, t_point speed);
-
-#endif
+void	sfe_move(t_sfe *sfe, t_point *pos, t_point speed)
+{
+	(void)sfe;
+	*pos = add_point(*pos, speed);
+}
