39607c057eec23654bac28db4538792f091fe8f1
Alienxbe <martin.ykman@gmail.com>
2023-11-30 17:38:37 +0100

    Added two alias hooking functions

---
+++ srcs/event/sfe_hook_on_keydown.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sfe_hook_on_keydown.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marykman <marykman@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 17:32:54 by marykman          #+#    #+#             */
+/*   Updated: 2023/11/30 17:34:56 by marykman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx.h"
+#include "t_window.h"
+#include "sfe_event.h"
+
+void	sfe_hook_on_keydown(t_window win, int (*f)(), void *param)
+{
+	if (win.win_ptr && f)
+		mlx_hook(win.win_ptr, ON_KEYDOWN, (1L<<0), f, param);
+}
---
+++ srcs/event/sfe_hook_on_keyup.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sfe_hook_on_keyup.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marykman <marykman@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 17:36:04 by marykman          #+#    #+#             */
+/*   Updated: 2023/11/30 17:36:20 by marykman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx.h"
+#include "t_window.h"
+#include "sfe_event.h"
+
+void	sfe_hook_on_keyup(t_window win, int (*f)(), void *param)
+{
+	if (win.win_ptr && f)
+		mlx_hook(win.win_ptr, ON_KEYUP, (1L<<1), f, param);
+}
